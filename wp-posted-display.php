8eb6ddaeca1242ba7940f87514ebf9fd9be063f1
Kazuya Takami <foundationmeister@outlook.com>
2018-06-27 14:00:42 +0900

    Typo Error. #133

--- wp-posted-display.php
+++ wp-posted-display.php
@@ -172,12 +172,12 @@ class Posted_Display {
 	/**
 	 * admin_scripts
 	 *
-	 * @version 2.0.6
+	 * @version 2.1.9
 	 * @since   1.1.0
 	 */
 	public function admin_scripts () {
-		wp_enqueue_script( 'wp-posted-display-admin-main-js'     , plugins_url( 'js/main.min.js', __FILE__ ), array('jquery'), $this->version );
-		wp_enqueue_script( 'wp-structuring-markup-admin-media-js', plugins_url( 'js/media-uploader-main.js', __FILE__ ), array( 'jquery' ), $this->version );
+		wp_enqueue_script( 'wp-posted-display-admin-main-js',  plugins_url( 'js/main.min.js', __FILE__ ), array('jquery'), $this->version );
+		wp_enqueue_script( 'wp-posted-display-admin-media-js', plugins_url( 'js/media-uploader-main.js', __FILE__ ), array( 'jquery' ), $this->version );
 		wp_enqueue_media();
 	}
 
